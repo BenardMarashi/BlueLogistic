9b262be2b94f034309bff2c02b08153f9e923a60
BenardMarashi <ben.marashi28@gmail.com>
2025-12-29 13:47:32 +0100

    initial commit

---
+++ blue-logistic/src/main/java/com/bluelogistic/BlueLogisticApplication.java
@@ -0,0 +1,13 @@
+package com.bluelogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLogisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueLogisticApplication.class, args);
+	}
+
+}
---
+++ blue-logistic/src/test/java/com/bluelogistic/BlueLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluelogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
